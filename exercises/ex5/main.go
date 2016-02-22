b164548fefd1935890c90cd052a72e0b8b9789ff
Chrislock1 <fredrikcch@msn.com>
2016-02-22 15:05:18 +0100

    new changes

--- exercises/ex5/main.go
+++ exercises/ex5/main.go
@@ -4,32 +4,44 @@ package main
 import (
 	"fmt"
 	"log"
-	//"runtime"
+	"runtime"
 )
 import "./driver"
 
 func main() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	//
-	//buttonChannel := make(chan driver.ElevButton, 10)
-	//lightChannel := make(chan driver.ElevLight)
 
+
+	//init hardware
 	log.Println("Main: \t Start in main")
+	buttonChannel := make(chan driver.ElevButton, 10)
+	lightChannel := make(chan driver.ElevLight)
+	motorChannel := make(chan int)
+	floorChannel := make(chan int)
 
-	driver.ElevInit()
+
+	if err := driver.ElevInit(buttonChannel,lightChannel, floorChannel); err != nil{
+		log.Println("ERROR -> Main: \t Hardware init failure")
+		log.Fatal(err)
+	}else{
+		log.Println("Hardware init complete")
+	}
 
 	fmt.Println("Press STOP button to stop the elevator and exit the program\n")
 
-	driver.ElevSetMotorDirection(driver.DIRN_UP)
+	// driver.ElevSetMotorDirection(driver.DIRN_UP)
 
 	for {
 
+		
+/*			Simple up and down test
 		if driver.ElevGetFloorSensorSignal() == driver.N_FLOORS-1 {
 			driver.ElevSetMotorDirection(driver.DIRN_DOWN)
 		} else if driver.ElevGetFloorSensorSignal() == 0 {
 			driver.ElevSetMotorDirection(driver.DIRN_UP)
 		}
-
+*/
 		if driver.ElevGetStopSignal() {
 			driver.ElevSetMotorDirection(driver.DIRN_STOP)
 
