ecd153794e0fb302bf943585556e305483245561
Chrislock1 <fredrikcch@msn.com>
2016-02-23 11:49:46 +0100

    added new things to try making it work

--- exercises/ex5/driver/elev.go
+++ exercises/ex5/driver/elev.go
@@ -5,12 +5,14 @@ package driver
 #cgo LDFLAGS: -lcomedi -lm
 #include "elev.h"
 #include "elev.c"
+
 */
 
 import (
 	"C"
 	"log"
 	"time"
+
 )
 
 const (
@@ -18,12 +20,35 @@ const (
 	N_BUTTONS = 3 //Number of buttons/lamps on a per-floor basis
 )
 
+
 //
 const (
-	BUTTON_CALL_UP   = 0
-	BUTTON_CALL_DOWN = 1
-	BUTTON_COMMAND   = 2
+	BUTTON_CALL_UP   = iota 
+	BUTTON_CALL_DOWN 
+	BUTTON_COMMAND   
+	SENSOR_FLOOR 
+	INDICATOR_FLOOR
+	BUTTON_STOP
+	SENSOR_OBST
+	INDICATOR_DOOR
 )
+
+var ButtonType =[]string{
+	"BUTTON_CALL_UP", 
+	"BUTTON_CALL_DOWN", 
+	"BUTTON_COMMAND",   
+	"SENSOR_FLOOR", 
+	"INDICATOR_FLOOR",
+	"BUTTON_STOP",
+	"SENSOR_OBST",
+	"INDICATOR_DOOR",
+}
+
+var MotorCmd = [3]string{
+	"DOWN",
+	"STOP",
+	"UP",
+}
 
 
 const elevStopDelay = 50 * time.Millisecond
@@ -49,14 +74,14 @@ type ElevLight struct {
 	Active bool
 }
 
-var lampChannelMatrix = [N_FLOORS][N_BUTTONS]int{
+var lampChannelMatrix = [N_FLOORS][3]int{
 	{LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
 	{LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
 	{LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
 	{LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
 }
 
-var buttonChannelMatrix = [N_FLOORS][N_BUTTONS]int{
+var buttonChannelMatrix = [N_FLOORS][3]int{
 	{BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
 	{BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
 	{BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
@@ -72,7 +97,7 @@ func ElevInit(buttonchannel chan<- ElevButton, lightChannel <-chan ElevLight, mo
 
 	clearAlllights()
 	go lightCheck(lightChannel)
-	go ElevSetMotorDirection(motorChannel)
+	go elevSetMotorDirection(motorChannel)
 	if getFloorSensorSignal() == -1 {
 		motorChannel <- DOWN
 		for {
@@ -80,13 +105,56 @@ func ElevInit(buttonchannel chan<- ElevButton, lightChannel <-chan ElevLight, mo
 				motorChannel <- STOP
 				break
 			} else {
-				time.sleep(elevDelay)
+				time.Sleep(elevDelay)
 			}
 		}
 	}
 	go readInput(buttonchannel, elevDelay)
 	go readFloorSensor(floorChannel, elevDelay)
 	return nil
+}
+
+func readInput(buttonchannel chan<- ElevButton, elevDelay time.Duration) {
+	inputMatrix := [N_FLOORS][3]bool{}
+	var stopbtn bool = false
+	for {
+		for Type := BUTTON_CALL_UP; Type <= BUTTON_COMMAND; Type++ {
+			for Floor := 0; Floor < N_FLOORS; Floor++ {
+				tempbtn := IoReadBit(buttonChannelMatrix[Floor][Type])
+				if tempbtn { // button been pressed
+					if !inputMatrix[Floor][Type] {
+						inputMatrix[Floor][Type] = true
+						buttonchannel <- ElevButton{Type, Floor}
+					}
+				} else {
+					inputMatrix[Floor][Type] = false
+				}
+			}
+		}
+		if IoReadBit(STOP_BUTTON) {
+			if !stopbtn {
+				stopbtn = true
+				buttonchannel <- ElevButton{Type: BUTTON_STOP}
+			}
+		} else {
+			stopbtn = false
+		}
+		time.Sleep(elevDelay)
+}
+
+
+}
+func readFloorSensor(floorChannel chan<- int, elevDelay time.Duration) {
+	var lastFloor int = -1
+	for {
+		tempFloor := getFloorSensorSignal()
+		if (tempFloor != -1) && (tempFloor != lastFloor) {
+			lastFloor = tempFloor
+			setFloorIndicator(tempFloor)
+			floorChannel <- tempFloor
+		}
+		time.Sleep(elevDelay)
+	}
 }
 
 func lightCheck(lightChannel <-chan ElevLight) {
@@ -127,7 +195,7 @@ func elevSetMotorDirection(motorChannel <-chan int) {
 		case cmd := <-motorChannel:
 			switch cmd {
 			case STOP:
-				time.sleep(elevatorStopDelay)
+				time.Sleep(elevStopDelay)
 				IoWriteAnalog(MOTOR, 0)
 			case UP:
 				IoClearBit(MOTORDIR)
@@ -142,96 +210,6 @@ func elevSetMotorDirection(motorChannel <-chan int) {
 	}
 }
 
-func readInput(buttonchannel chan<- ElevButton, elevdelay time.Duration) {
-	inputMatrix := [N_FLOORS][N_BUTTONS]bool{}
-	var stopbtn bool = false
-	for {
-		for Type := BUTTON_CALL_UP; Type <= BUTTON_COMMAND; Type++ {
-			for Floor := 0; Floor < N_FLOORS; Floor++ {
-				tempbtn := IoReadBit(buttonChannelMatrix[Floor][Type])
-				if tempbtn { // button been pressed
-					if !inputMatrix[Floor][Type] {
-						inputMatrix[Floor][Type] = true
-						buttonchannel <- ElevButton{Type, Floor}
-					}
-				} else {
-					inputMatrix[Floor][Type] = false
-				}
-			}
-		}
-		if IoReadBit(STOP_BUTTON) {
-			if !stopbtn {
-				stopbtn = true
-				buttonchannel <- ElevButton{Type: BUTTON_STOP}
-			}
-		} else {
-			stopbtn = false
-		}
-		time.sleep(elevdelay)
-}
-
-func lightCheck(lightChannel <- chan ElevLight){
-	var cmd ElevLight
-	for {
-		select{
-			case cmd = <-lightChannel:
-				switch cmd.Type{
-				case BUTTON_STOP:
-					if cmd.Active{
-						IoSetBit(LIGHT_STOP)
-					} else{
-						IoClearBit(LIGHT_STOP)
-					}
-				case BUTTON_CALL_UP, BUTTON_CALL_DOWN, BUTTON_COMMAND:
-					if cmd.Active{
-						IoSetBit(lampChannelMatrix[cmd.Floor][cmd.Type])
-					}else {
-						IoClearBit(lampChannelMatrix[cmd.Floor][cmd.Type])
-					}
-				case INDICATOR_DOOR:
-					if cmd.Active{
-						IoSetBit(LIGHT_DOOR_OPEN)
-					}else{
-						IoClearBit(LIGHT_DOOR_OPEN)
-					}
-				default:
-					log.Println("Elev: \t You tried to light a non light item")
-				}
-		}
-		
-	}
-}
-
-
-
-
-
-
-func readFloorSensor(floorChannel chan<- int, elevDelay time.Duration) {
-	var lastFloor int = -1
-	for {
-		tempFloor := getFloorSensorSignal()
-		if (tempFloor != -1) && (tempFloor != lastFloor) {
-			lastFloor = tempFloor
-			setFloorIndicator(tempFloor)
-			floorChannel <- tempFloor
-		}
-		time.sleep(elevDelay)
-	}
-}
-
-
-func getFloorSensorSignal() int {
-	if IoReadBit(SENSOR_FLOOR1){
-		return 0
-	}else if IoReadBit(SENSOR_FLOOR2){
-		return 1
-	}else if IoReadBit(SENSOR_FLOOR3){
-		return 2
-	}else if IoReadBit(SENSOR_FLOOR4){
-		return 3
-	}else{return -1}
-}
 
 func setFloorIndicator(floor int) {
 	if floor >= N_FLOORS{
@@ -254,9 +232,23 @@ func setFloorIndicator(floor int) {
 	}
 }
 
-func ElevGetStopSignal() bool { 
-	return IoReadBit(STOP)
+func getFloorSensorSignal() int {
+		if IoReadBit(SENSOR_FLOOR1) {
+				return 0
+		}else if IoReadBit(SENSOR_FLOOR2){
+			return 1
+		}else if IoReadBit(SENSOR_FLOOR3){
+			return 2
+		}else if IoReadBit(SENSOR_FLOOR4){
+			return 3
+		}else{return -1}
 }
+
+
+
+//func ElevGetStopSignal() bool { 
+//	return IoReadBit(STOP)
+//}
 
 func ElevGetObstructuionSignal() bool { //not going to use
 	return IoReadBit(OBSTRUCTION)
