4ad04f391728bc8e5d75c53660a00e90d8e36560
Abortkleshengeren <sales@scrapeitout.com>
2016-02-24 18:25:41 +0100

    working file

--- exercises/ex6/backup.go
+++ exercises/ex6/backup.go
@@ -1,47 +1,46 @@
 package main
 
-import (
-	"encoding/binary"
+import(
 	"log"
 	"net"
-	"os/exec"
 	"time"
+	"encoding/binary"
+	"os/exec"
 )
 
-func primary(start int, udpBroadcast *net.UDPConn) {
-	//Filename := "cmd", "/C", "start", "primary.exe"
-	newBackup := exec.Command("gnome-terminal", "-x", "sh", "-c", "go run backup.go") //gnome-terminal", "-x", "sh", "-c", "go run backup.go
+func primary(start int, udpBroadcast *net.UDPConn){
+
+	newBackup := exec.Command("gnome-terminal", "-x", "sh", "-c", "go run backup.go")
 	err := newBackup.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil {log.Fatal(err)}
 
 	msg := make([]byte, 1)
 
-	for i := start; ; i++ {
+	for i := start;; i++{
 		log.Println(i)
-		msg[0] = byte(i)
+		msg[0] = byte(i);
 		udpBroadcast.Write(msg)
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(100*time.Millisecond)
 	}
-
+	
 }
 
-func backup(udpListen *net.UDPConn) int {
-	listenChan := make(chan int, 1)
+func backup(udpListen *net.UDPConn) int{
+	listenChan := make(chan int, 1); 
 	backupvalue := 0
 	go listen(listenChan, udpListen)
 	for {
 		select {
-		case backupvalue = <-listenChan:
-			time.Sleep(50 * time.Millisecond)
-			break
-		case <-time.After(1 * time.Second):
-			log.Println("The primary is dead, long live the primary")
-			return backupvalue
+			case backupvalue = <- listenChan:
+				time.Sleep(50*time.Millisecond)
+				break
+			case <-time.After(1*time.Second):
+				log.Println("The primary is dead, long live the primary")
+				return backupvalue
 		}
 	}
-
+	
+	
 }
 
 func listen(listenChan chan int, udpListen *net.UDPConn) {
@@ -50,42 +49,36 @@ func listen(listenChan chan int, udpListen *net.UDPConn) {
 
 	for {
 		udpListen.ReadFromUDP(buffer[:])
-		//if err != nil {log.Fatal(err)}
-
+		//if err != nil {log.Fatal(err)} 
+		
 		listenChan <- int(binary.LittleEndian.Uint64(buffer)) //convert an bytearray to int
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(100*time.Millisecond)
 	}
-
+	
 }
 
 func main() {
-
-	udpAddr, err := net.ResolveUDPAddr("udp", "129.241.187.255:20015")
-	if err != nil {
-		log.Fatal(err)
-	}
+	
+	udpAddr, err := net.ResolveUDPAddr("udp", ":20015")
+	if err != nil {log.Fatal(err)}
 
 	udpListen, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	if err != nil {log.Fatal(err)}
+	
 	backupvalue := backup(udpListen)
-
+	
 	udpListen.Close()
-
-	udpAddr, err = net.ResolveUDPAddr("udp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
+	
+	udpAddr, err = net.ResolveUDPAddr("udp","129.241.187.255:20015")
+	if err != nil {log.Fatal(err)}
 
 	udpBroadcast, err := net.DialUDP("udp", nil, udpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	if err != nil {log.Fatal(err)}
+	
+	
 	primary(backupvalue, udpBroadcast)
-
+	
 	udpBroadcast.Close()
-
+	
+	
 }
