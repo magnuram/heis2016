34255547de67c4f410fdb42c19425c08d998711d
martinSilverPC <martin_lesund@hotmail.com>
2016-04-06 15:15:41 +0200

    updating

--- project/src/network/network.go
+++ project/src/network/network.go
@@ -9,3 +9,13 @@ import (
 
 var LocAddr *net.UDPAddr //Local address
 var BroAddr *net.UDPAddr //Broadcast address
+
+type Udp_message struct {
+	Raddr  string //if receiving raddr=senders address, if sending raddr should be set to "broadcast" or an ip:port
+	Data   []byte //TODO: implement another encoding, strings are meh
+	Length int    //length of received data, in #bytes // N/A for sending
+}
+
+func UdpInit(localListenPort, broadcastListenPort, msgSize int, sendChan, recieveChan chan UdpMessage)(err error){
+
+}	Raddr  string //if r
