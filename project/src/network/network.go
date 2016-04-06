ebaaea05f6972008a42e14f48c4451f382afb1b4
martinSilverPC <martin_lesund@hotmail.com>
2016-04-06 21:06:32 +0200

    new change meaow

--- project/src/network/network.go
+++ project/src/network/network.go
@@ -18,4 +18,4 @@ type Udp_message struct {
 
 func UdpInit(localListenPort, broadcastListenPort, msgSize int, sendChan, recieveChan chan UdpMessage)(err error){
 
-}	Raddr  string //if reading test
+}	Raddr  string //if reading tests
