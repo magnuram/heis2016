a9d99fa8da0b335238a909793395167d3701aabb
Abortkleshengeren <sales@scrapeitout.com>
2016-04-14 15:49:20 +0200

    ss

--- project/src/queue/queue.go
+++ project/src/queue/queue.go
@@ -3,3 +3,20 @@ package queue
 import (
 	"driver"
 )
+
+//var light driver.ElevLight
+var elevInfo driver.ElevInfo
+
+func QueueSetLights(lightChannel <-chan driver.ElevLight) {
+	var ligt driver.ElevLight
+	ligt = <-lightChannel
+	for i := 0; i < driver.N_FLOORS; i++ {
+		lightChannel <- ligt{Type: BUTTON_COMMAND, Floor: i, Active: elevInfo.ReqDown[i]}
+		if i != 0 {
+			lightChannel <- ligt{Type: BUTTON_CALL_DOWN, Floor: i, Active: elevInfo.ReqUp[i]}
+		}
+		if i != 3 {
+			lightChannel <- ligt{Type: BUTTON_CALL_DOWN, Floor: i, Active: elevInfo.ReqUp[i]}
+		}
+	}
+}
