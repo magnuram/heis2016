c3c589feb2da1cc9fab9d0d26662f738efb54a3b
martiles <kissahen@gmail.com>
2016-04-17 20:13:25 +0200

    added simple network.. not tested yet

--- project/src/network/network.go
+++ project/src/network/network.go
@@ -1,96 +1,69 @@
 package network
 
 import (
-    . "../config"
-    "../udp"
-    "encoding/json"
-    "log"
-)
+    . "./src/udp"
+    "time"
+    "net"
+  
+    "fmt"
+    )
 
-const debug = false
+var elev_timers map[int]*time.Timer
 
-func Init(reciveOrderChannel chan<- ElevOrderMessage,
-    sendOrderChannel <-chan ElevOrderMessage,
-    reciveRestoreChannel chan<- ElevRestoreMessage,
-    sendRestoreChannel <-chan ElevRestoreMessage) (localIP string, err error) {
-    const messageSize = 4 * 1024
-    const UDPLocalListenPort = 20002
-    const UDPBroadcastListenPort = 20007
-    UDPSendChannel := make(chan udp.UDPMessage, 10)
-    UDPReceiveChannel := make(chan udp.UDPMessage)
-    localIP, err = udp.Init(UDPLocalListenPort, UDPBroadcastListenPort, messageSize, UDPSendChannel, UDPReceiveChannel)
-    if err != nil {
-        return "", err
+func broadCastIP(ID int, UDPsend chan Message) {
+    for {
+        UDPsend <- Message{Source: ID, Id: IP}
+        time.Sleep(100 * time.Millisecond)
     }
-    go reciveMessageHandler(reciveOrderChannel, reciveRestoreChannel, UDPReceiveChannel)
-    go sendMessageHandler(sendOrderChannel, sendRestoreChannel, UDPSendChannel)
-    return localIP, nil
 }
 
-func reciveMessageHandler(reciveOrderChannel chan<- ElevOrderMessage, reciveRestoreChannel chan<- ElevRestoreMessage, UDPReceiveChannel <-chan udp.UDPMessage) {
+func Network_manager(from_main chan Message, to_main chan Message) {
+    addr, _ := net.InterfaceAddrs()
+    self_id := int(addr[1].String()[12] - '0') * 100 + int(addr[1].String()[13] - '0') * 10 + int(addr[1].String()[14] - '0')
+
+    UDPsend := make(chan Message, 100)
+    UDPreceive := make(chan Message, 100)
+
+    
+    go broadCastIP(self_id, UDPsend)
+    go UDPListener(UDPreceive)
+    go UDPSender(UDPsend)
+
+    elev_timers = make(map[int]*time.Timer)
+    
     for {
         select {
-        case msg := <-UDPReceiveChannel:
-            var f interface{}
-            err := json.Unmarshal(msg.Data[:msg.Length], &f)
-            if err != nil {
-               // printDebug("Error with Unmarshaling a message.")
-                log.Println(err)
-            } else {
-                m := f.(map[string]interface{})
-                event := int(m["Event"].(float64))
-                if event <= 3 && event >= 0 {
-                    var restore = ElevRestoreMessage{}
-                    if err := json.Unmarshal(msg.Data[:msg.Length], &restore); err == nil {
-                        if restore.IsValid() {
-                            reciveRestoreChannel <- restore
-                            //printDebug("Recived an ElevRestoreMessage with Event " + EventType[restore.Event])
+            case message := <- UDPreceive:
+                _, present := elev_timers[message.Source]
+
+                if message.Id == IP {
+                    if message.Source != self_id {
+                        if present {
+                            elev_timers[message.Source].Reset(time.Second)
                         } else {
-                           // printDebug("Rejected an ElevRestoreMessage with Event " + EventType[restore.Event])
-                        }
-                    } else {
-                        //printDebug("Error with Unmarshaling a ElevStateMessage")
-                    }
-                } else if event >= 4 && event <= 10 {
-                    var order = ElevOrderMessage{}
-                    if err := json.Unmarshal(msg.Data[:msg.Length], &order); err == nil {
-                        if order.IsValid() {
-                            reciveOrderChannel <- order
-                            //printDebug("Recived an ElevOrderMessage with Event " + EventType[order.Event])
-                        } else {
-                            //printDebug("Rejected an ElevOrderMessage with Event " + EventType[order.Event])
+                            elev_timers[message.Source] = time.AfterFunc(time.Second, func() { remove_elev(message.Source, from_main) } )
+                            to_main <- Message{Source: message.Source, Id: ELEVATOR_ADDED_TO_NTWK}
                         }
                     }
-                } else {
-                  //  printDebug("Recived an unknown message type")
+                    break
                 }
-            }
+
+                if message.Checksum != message.Calc_checksum() {
+                    fmt.Println("CHECKSUM ERROR", message.Checksum, " vs ", message.Calc_checksum()) 
+                    break
+                }
+
+                to_main <- message
+
+            case message := <- from_main:
+                message.Checksum = message.Calc_checksum()
+                UDPsend <- message
         }
+
     }
 }
 
-func sendMessageHandler(sendOrderChannel <-chan ElevOrderMessage, sendRestoreChannel <-chan ElevRestoreMessage, UDPSendChannel chan<- udp.UDPMessage) {
-    for {
-        select {
-        case msg := <-sendOrderChannel:
-            networkPack, err := json.Marshal(msg)
-            if err != nil {
-                //printDebug("Error Marshalling an outgoing message")
-                log.Println(err)
-            } else {
-                UDPSendChannel <- udp.UDPMessage{Raddr: "broadcast", Data: networkPack}
-               // printDebug("Sent an ElevOrderMessage with Event " + EventType[msg.Event])
-            }
-
-        case msg := <-sendRestoreChannel:
-            networkPack, err := json.Marshal(msg)
-            if err != nil {
-               // printDebug("Error Marshalling an outgoing message")
-                log.Println(err)
-            } else {
-                UDPSendChannel <- udp.UDPMessage{Raddr: "broadcast", Data: networkPack}
-                //printDebug("Sent an ElevOrderMessage with Event " + EventType[msg.Event])
-            }
-        }
-    }
+func remove_elev(id int, to_main chan Message) {
+    delete(elev_timers, id)
+    to_main <- Message{Source: id, Id: ELEVATOR_REMOVED_FROM_NTWK}
 }
